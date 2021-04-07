8e39b618c239453f365b8ebb947cbeaa8bddd705
Robby Noyes <robertjnoyes1@gmail.com>
2021-04-06 22:03:16 -0700

    codewars April 6 2021

--- codewars-04-week1/js/main.js
+++ codewars-04-week1/js/main.js
@@ -2,16 +2,23 @@
 
 function findOdd(A) {
   for (let i = 0; i < A.length; i++) {
-    let count = 0
+    let count = 0;
     for (let j = 0; j < A.length; j++) {
-      if (A[i] === A[j]){
-        count++
+      if (A[i] === A[j]) {
+        count++;
       }
     }
-    if (count % 2 === 0){
-      count = 0
-    } else return A[i]
+    if (count % 2 === 0) {
+      count = 0;
+    } else return A[i];
   }
 }
 
-console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5,4]))
+console.log(
+  findOdd([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5, 4])
+);
+
+//Is this a triangle?
+function isTriangle(a,b,c) {
+  return a + b > c && b + c > a && a + c > b
+}

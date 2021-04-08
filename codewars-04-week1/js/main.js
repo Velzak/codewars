658ae0c57deffe9af71fc19e93babe8ab8a0cd7e
Robby Noyes <robertjnoyes1@gmail.com>
2021-04-07 22:14:00 -0700

    codewars April 7 2021

--- codewars-04-week1/js/main.js
+++ codewars-04-week1/js/main.js
@@ -22,3 +22,30 @@ console.log(
 function isTriangle(a,b,c) {
   return a + b > c && b + c > a && a + c > b
 }
+
+//Create a Like System 
+
+function likes(names) {
+  switch (names.length) {
+    case 0:
+      return `no one likes this`
+    case 1:
+      return `${names[0]} likes this`
+    case 2:
+      return `${names[0]} and ${names[1]} like this`
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`
+    default:
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+  }
+}
+
+console.log(likes(["Alex", "Jacob", "Mark", "Max", '']))
+
+//Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+function disemvowel(str) {
+  return str.replace(/[aeiou]/ig,'')
+}
+
+console.log(disemvowel(("This website is for losers LOL!")))

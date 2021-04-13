23c9b8281e95cb0a26169056c759eb3a6b4c2f49
Robby Noyes <robertjnoyes1@gmail.com>
2021-04-12 21:32:42 -0700

    codewars April 12 2021

--- codewars-04-week2/js/main.js
+++ codewars-04-week2/js/main.js
@@ -25,3 +25,29 @@ function nbMonths(startPriceOld, startPriceNew, savingperMonth, percentLossByMon
 }
 
 console.log(nbMonths(2000, 8000, 1000, 1.5));
+
+
+//Build Tower 
+//Checked Solution, having trouble with Kata
+function towerBuilder(nFloors) {
+  let arr = []
+  let star = 1
+  for (let i = 1; i <= nFloors; i++) {
+    arr[i-1] = " ".repeat(nFloors-i) + "*".repeat(star) + " ".repeat(nFloors - i)
+    star += 2
+  }
+  return arr
+}
+
+console.log(towerBuilder(2))
+
+function toTime(seconds) {
+  let hours = Math.floor(seconds / 60 / 60)
+  let minutes = Math.floor(seconds / 60)
+  if (minutes >= 60){
+    minutes = minutes - (hours * 60)
+  }
+  return `${hours} hour(s) and ${minutes} minute(s)`
+}
+
+console.log(toTime(323500))

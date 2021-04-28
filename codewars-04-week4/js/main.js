a6abd6a3e63aee37de4d8ae4ac968d9ab51a38dd
Robby Noyes <robertjnoyes1@gmail.com>
2021-04-27 20:10:52 -0700

    codewars April 27 2021

--- codewars-04-week4/js/main.js
+++ codewars-04-week4/js/main.js
@@ -23,3 +23,16 @@ function dataReverse(data) {
   }
   return [].concat.apply([], newArr)
 }
+
+
+function solution(number){
+  let sum = 0
+  for (let i = 0; i < number; i++) {
+    if(i % 3 === 0 || i % 5 === 0){
+      sum += i
+    } 
+  }
+  return sum
+}
+
+console.log(solution(10))

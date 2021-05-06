5944539d780e698a9ccc5e966bd50b2c6e7fcf4a
Robby Noyes <robertjnoyes1@gmail.com>
2021-05-05 22:29:16 -0700

    codewars May 5 2021

--- codewars-05-week1/js/main.js
+++ codewars-05-week1/js/main.js
@@ -19,3 +19,12 @@ function solution(nums){
 
 
 console.log(solution([0, 3, 1]))
+
+var number = function(busStops){
+  let peopleOnBus = 0
+  for (let i = 0; i < busStops.length; i++) {
+      peopleOnBus += busStops[i][0]
+      peopleOnBus -= busStops[i][1]
+  }
+  return peopleOnBus
+}

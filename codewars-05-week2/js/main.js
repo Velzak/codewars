3be03261bda9442cd6198e117ba7dc4393eda18a
Robby Noyes <robertjnoyes1@gmail.com>
2021-05-10 22:48:16 -0700

    codewars May 10 2021

--- codewars-05-week2/js/main.js
+++ codewars-05-week2/js/main.js
@@ -2,3 +2,8 @@ function filter_list(l) {
   return l.filter(num => Number.isFinite(num) === true && num >= 0)
 }
 
+function duplicateCount(text){
+  return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+}
+
+console.log(duplicateCount('aabBbbcde'))

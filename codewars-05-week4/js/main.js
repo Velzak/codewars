b9d68e2f043ac42d42d972fb29fe6d1646263d2a
Robby Noyes <robertjnoyes1@gmail.com>
2021-05-26 23:44:41 -0700

    codewars May 26 2021

--- codewars-05-week4/js/main.js
+++ codewars-05-week4/js/main.js
@@ -1,24 +1,39 @@
 function persistence(num) {
-  let times = 0
-  num = num.toString()
-  while (num.length > 1){
-    times++
-    num = num.split('').map(Number).reduce((a,b) => a * b).toString()
+  let times = 0;
+  num = num.toString();
+  while (num.length > 1) {
+    times++;
+    num = num
+      .split("")
+      .map(Number)
+      .reduce((a, b) => a * b)
+      .toString();
   }
-  return times
+  return times;
 }
 
-console.log(persistence(39))
+console.log(persistence(39));
 
-function toCamelCase(str){
-  let strSplice = str.split('')
+function toCamelCase(str) {
+  let strSplice = str.split("");
   for (let i = 0; i < strSplice.length; i++) {
-    if(strSplice[i] === '-' || strSplice[i] === '_'){
-      strSplice.splice(i, 1)
-      strSplice[i] = strSplice[i].charAt(0).toUpperCase()
+    if (strSplice[i] === "-" || strSplice[i] === "_") {
+      strSplice.splice(i, 1);
+      strSplice[i] = strSplice[i].charAt(0).toUpperCase();
     }
   }
-  return strSplice.join('')
+  return strSplice.join("");
 }
 
-console.log(toCamelCase("the_stealth_warrior"))
+console.log(toCamelCase("the_stealth_warrior"));
+
+function evenLast(numbers) {
+  if (numbers.length === 0) return 0;
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i += 2) {
+    sum += numbers[i];
+  }
+  return sum * numbers[numbers.length - 1];
+}
+
+console.log(evenLast([2, 2, 2, 2]));

0d1826f75cb3121f7ecd92be0fe51280964010a7
Robby Noyes <robertjnoyes1@gmail.com>
2021-05-27 10:45:42 -0700

    codewars May 27 2021

--- codewars-05-week4/js/main.js
+++ codewars-05-week4/js/main.js
@@ -37,3 +37,10 @@ function evenLast(numbers) {
 }
 
 console.log(evenLast([2, 2, 2, 2]));
+
+function createPhoneNumber(numbers) {
+  numbers = numbers.join('')
+  return `(${numbers.substring(0,3)}) ${numbers.substring(3,6)}-${numbers.substring(6,10)}`
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
